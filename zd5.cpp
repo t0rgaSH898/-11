7ed130f4b5ce12e36eb084dc3790f37a1dd77ef1
Владислав Торганенко <vladtorg.anatol@gmail.com>
2026-04-09 11:52:09 +0300

    Update zd5.cpp

--- zd5.cpp
+++ zd5.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    int N;
-    long long factorial = 1; 
+    setlocale(LC_ALL, "Russian");
 
-    cin >> N;
-    
-    if (N < 0) {
-        return 1;
-    }
-    
-    for (int i = 1; i <= N; i++) {
+    int n;
+    cin >> n;
+
+    long long factorial = 1;
+    for (int i = 1; i <= n; i++) {
         factorial *= i;
     }
-    
     cout << factorial << endl;
-    
+
     return 0;
 }
