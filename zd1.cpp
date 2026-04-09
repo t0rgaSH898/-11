0eb85b529989e866f14d00f49fc49c6b85a968f1
Владислав Торганенко <vladtorg.anatol@gmail.com>
2026-04-09 11:46:53 +0300

    Update zd1.cpp

--- zd1.cpp
+++ zd1.cpp
@@ -6,12 +6,13 @@ int main() {
     setlocale(LC_ALL, "Russian");
 
     int n;
-    int i;
     cin >> n;
 
-    for(i = 1; i <= n; i++)
-    { 
-        cout << i << " ";
+    for (int i = 1; i <= n; i++) {
+        if (i > 1) cout << " ";
+        cout << i;
     }
+    cout << endl;
+
     return 0;
 }
